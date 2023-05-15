44ab05aa0239069aeca72a99f0fe22357414bcbe
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-05-14 21:00:49 -0300

    feat(minishelly) criação de funções para ajudar na reordenação dos tokens

--- srcs/interfaces/minishelly.c
+++ srcs/interfaces/minishelly.c
@@ -6,7 +6,7 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 23:21:09 by msilva-p          #+#    #+#             */
-/*   Updated: 2023/05/12 12:48:28 by dapaulin         ###   ########.fr       */
+/*   Updated: 2023/05/14 20:57:35 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,81 @@ static void	args_check(int argc)
 	}
 }
 
+void	swap_tokens(t_token *bk, t_token **md, t_token *end)
+{
+	t_token	*tmp;
+
+	tmp = *md;
+	if (bk)
+		bk->next = end;
+	tmp->next = end->next;
+	end->next = tmp;
+	//*md = end;
+}
+
+void	copy_token(t_token *bk, t_token **md, t_token *end)
+{
+	t_token	*cpy;
+
+	cpy = ft_token_new(end->token, end->type);
+	if (bk)
+		bk->next = cpy;
+	cpy->next = *md;
+	//*md = cpy;
+}
+
+char **ft_realloc(char **array)
+{
+	int		i;
+	char	**tmp;
+
+	i = 0;
+	while (array[i])
+		i++;
+	tmp = ft_calloc(sizeof(char *), i);
+	i = 1;
+	while (array[i])
+	{
+		tmp[i - 1] = array[i];
+		i++;
+	}
+	if (array)
+		free(array);
+	return (tmp);
+}
+
+void	correct_puts(t_token *md, t_token *end)
+{
+	char	**tmp;
+	
+	tmp = NULL;
+	if (md->type >= 4 && md->type <= 7)
+	{
+		tmp = ft_calloc(sizeof(char *), 3);
+		tmp[0] = md->token[0];
+		tmp[1] = end->token[0];
+		if (md->token)
+			free(md->token);
+		md->token = tmp;
+		end->token = ft_realloc(end->token);
+	}
+}
+
 //função que faz a identificação dos tipos dos tokens
 void	print_tokens_test(t_token *tokens)
 {
-	int	i;
+	int		i;
+	t_token	*back;
 
+	back = NULL;
+	correct_puts(tokens, tokens->next);
 	while (tokens)
 	{
 		i = 0;
-		printf("Operador: %i\n[", tokens->type);
+		ft_printf("Operador: %i\n[", tokens->type);
 		while (tokens->token[i])
 		{
-			printf("'%s', ", tokens->token[i]);
+			ft_printf("'%s', ", tokens->token[i]);
 			i++;
 		}
 		printf("]\n");
@@ -59,6 +122,7 @@ int	minishelly(int argc, char **argv, char **environ)
 			continue ;
 		search_for_symbol(&mini->new_parser, '$', mini->env);
 		mini->tokens = ft_create_tokens(mini);
+		print_tokens_test(mini->tokens);
 		exec(mini);
 		add_history(mini->str);
 		ft_token_free(&mini->tokens);
