acf7f54c468ca1143df614798939d51e68f9239c
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-05-24 13:32:39 -0300

    Refactory(ft_output): refatoração da função ft_output e implementação de uma função, fd_output_validation, para validação de access do arquivo

--- srcs/operators/ft_output.c
+++ srcs/operators/ft_output.c
@@ -6,11 +6,33 @@
 /*   By: dapaulin <dapaulin@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 12:03:37 by dapaulin          #+#    #+#             */
-/*   Updated: 2023/05/24 10:28:26 by dapaulin         ###   ########.fr       */
+/*   Updated: 2023/05/24 13:27:21 by dapaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+static int	fd_output_validation(int *fd, char *token)
+{
+	if (run_access(token, W_OK))
+	{
+		*fd = open(token, O_WRONLY | O_TRUNC);
+		if (*fd == -1)
+			return (ft_exit_in_out_error(1));
+	}
+	else
+	{
+		if (*get_status_code() == 126)
+		{
+			*fd = open(token, O_WRONLY | O_CREAT, 0644);
+			if (*fd == -1)
+				return (ft_exit_in_out_error(1));
+		}
+		else
+			return (ft_exit_in_out_error(1));
+	}
+	return (0);
+}
 
 int	ft_output(t_sys_config *ms)
 {
@@ -18,23 +40,8 @@ int	ft_output(t_sys_config *ms)
 	t_process_func	*func;
 
 	func = ms->exec->func;
-	if (run_access(ms->tokens->token[1], W_OK))
-	{
-		fd = open(ms->tokens->token[1], O_WRONLY | O_TRUNC);
-		if (fd == -1)
-			return (ft_exit_in_out_error(1));
-	}
-	else
-	{
-		if (*get_status_code() == 126)
-		{
-			fd = open(ms->tokens->token[1], O_WRONLY | O_CREAT, 0644);
-			if (fd == -1)
-				return (ft_exit_in_out_error(1));
-		}
-		else
-			return (ft_exit_in_out_error(1));
-	}
+	if (fd_output_validation(&fd, ms->tokens->token[1]))
+		return (1);
 	if (ms->tokens->next)
 	{
 		ms->tokens = ms->tokens->next;
